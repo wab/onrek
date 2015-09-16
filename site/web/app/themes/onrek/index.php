0d51dcd8c4b54f3b2e6a3dc6d6f222cfd0cb1416
wab <wab2com@gmail.com>
2015-09-16 14:32:55 +0200

    index ng-repeat test

--- site/web/app/themes/onrek/index.php
+++ site/web/app/themes/onrek/index.php
@@ -7,8 +7,12 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
-
+<?php
+	// while (have_posts()) : the_post();
+  // get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format());
+	//endwhile; 
+?>
+  <ul>
+    <li ng-repeat='post in main.posts'><a href="{{ post.slug }}">{{ post.title }}</a></li>
+  </ul>
 <?php the_posts_navigation(); ?>
